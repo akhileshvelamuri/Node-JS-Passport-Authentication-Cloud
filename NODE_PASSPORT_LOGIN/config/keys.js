3bc575debeb9893bfb67b74db52e5d05fb63281d
AkhileshVelamuri <35664937+AkhileshVelamuri@users.noreply.github.com>
2020-07-19 16:41:05 +0530

    Tested!

---
+++ NODE_PASSPORT_LOGIN/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //MongoURI: 'mongodb+srv://akhilesh_vela:baZ6vLf3PR4Bayty@sample-wedth.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false'
+}
